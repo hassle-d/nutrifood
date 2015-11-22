e4407fd2a19ee33a06bd07a8901ee9596e40825a
hassle-d <damien.hassler@epitech.eu>
2015-11-22 20:28:33 +0800

    Html files serving

--- src/server/routes/front.js
+++ src/server/routes/front.js
@@ -3,6 +3,7 @@
 */
 
 var express = require('express');
+var path 	= require('path');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
@@ -12,6 +13,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/index', function(req, res, next) {
   res.render( 'index', { title: 'Index' } );
+});
+
+router.get('/test', function(r, q, n) {
+	q.sendFile(path.join(__dirname, '../views/test.html'));
 });
 
 module.exports = router;
