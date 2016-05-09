8c24b309f0541f25aa1fd1cc33194f5649c985f9
RavielCD <dimitri.cisneros@epitech.eu>
2016-05-09 15:15:49 +0800

    correct

--- src/Android/app/src/main/java/com/nutrifood/cisner_d/nutrifood2/CallBack.java
+++
@@ -1,5 +0,0 @@
-package com.nutrifood.cisner_d.nutrifood2;
-
-public interface CallBack {
-    void onItemClicked(Object item);
-}
--- src/Android/app/src/main/java/com/nutrifood/cisner_d/nutrifood2/DataHolder.java
+++
@@ -1,8 +0,0 @@
-package com.nutrifood.cisner_d.nutrifood2;
-
-/**
- * Created by dimit_000 on 03/04/2016.
- */
-public class DataHolder {
-    public static String token = null;
-}
